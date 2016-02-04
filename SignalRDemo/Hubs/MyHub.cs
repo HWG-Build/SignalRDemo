2f8c714e8fb11188f39263d339cce767bc85ce36
Jonathan Lee <lee.yuchan@gmail.com>
2016-02-03 19:48:16 -0800

    Initial Commit

---
+++ SignalRDemo/Hubs/MyHub.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using Microsoft.AspNet.SignalR;
+
+namespace SignalRDemo.Hubs
+{
+    public class MyHub : Hub
+    {
+        public void Hello()
+        {
+        }
+
+        public override Task OnConnected()
+        {
+            string name = Context.ConnectionId;
+            Clients.All.addUserIcon(name);
+            return base.OnConnected();
+        }
+
+        public override Task OnDisconnected(bool b)
+        {
+            string name = Context.ConnectionId;
+            Clients.All.removeUserIcon(name);
+            return base.OnDisconnected(b);
+        }
+    }
+}
---
+++ SignalRDemo/Scripts/demo.js
@@ -0,0 +1,30 @@
+﻿$(function () {
+    $.connection.hub.logging = true;
+    var myhub = $.connection.myHub;
+    myhub.client.hello = function () {
+
+    };
+
+    myhub.client.addUserIcon = function (name) {
+        
+        $('#panelHead').append('<div><strong>' + name + '</strong> has joined the page. <div>')
+    }
+
+    myhub.client.removeUserIcon = function(name) {
+        $('#panelHead').append('<div><strong>' + name + '</strong> has left the page. <div>')
+    }
+
+    $.connection.hub.start().done(function() {
+        //myhub.server.OnConnected();
+       
+    });
+
+    //$.connection.hub.stop(function() {
+    //   myhub.server.OnDisconnected();
+    //});
+});
+
+$(document).ready(function () {
+    
+});
+
